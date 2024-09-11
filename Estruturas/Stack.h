beb9f26c8be3db074c343ff94cdab66ff2ff2894
Matheus Alencar Nascimento <matheuskater02@outlook.com>
2024-09-10 21:05:07 -0300

    versão 1.0.0

---
+++ Estruturas/Stack.h
@@ -0,0 +1,23 @@
+#ifndef STACK_H
+#define STACK_H
+
+const int MaxStack = 100;
+
+class Stack {
+    public:
+        Stack();
+        ~Stack();
+        bool Empty();
+        bool Full();
+        void Push(int x);
+        void Pop(int &x);
+        void Clear();
+        void Top(int &x);
+        int Size();
+    private:
+        int top;
+        int Entry[MaxStack-1];
+};
+
+#endif
+
